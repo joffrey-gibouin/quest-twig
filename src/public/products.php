b311fc3baa3d4e0fe04b505985a7845f31caf44a
joffrey-gibouin <joffreygibouin@gmail.com>
2021-10-19 09:59:48 +0200

    add products

--- src/public/products.php
+++ src/public/products.php
@@ -1,9 +1,10 @@
 <?php
-
-// Get a $twig object from this file.
-require_once __DIR__ . '/../config/twig.php';
+require_once __DIR__ . '/../../config/twig.php';
 
 
 $products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre'];
 
 echo $twig->render('products.html.twig', ['products' => $products]);
+
+
+
